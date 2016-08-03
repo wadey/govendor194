5441efda51de58291539ca365e8c92204570c431
Wade Simmons <wade@wades.im>
2016-08-03 10:48:33 -0400

    add test.sh script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e -x
+
+cd bravo
+rm -rf vendor
+govendor init
+govendor fetch -v +outside
+govendor list
+
+if govendor list +vendor | grep -q "github.com/wadey/govendor194"
+then
+    echo "failed: local package is present in vendor/"
+    exit 1
+fi
