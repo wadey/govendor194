55d5a5ea8a59118a05951f0686110754680d0f6c
Wade Simmons <wade@wades.im>
2016-08-04 08:48:49 -0400

    fix test

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ govendor init
 govendor fetch -v +outside
 govendor list
 
-if govendor list +vendor | grep -q "github.com/wadey/govendor194"
+if govendor list +vendor | grep -q "github.com/wadey/govendor194/bravo"
 then
     echo "failed: local package is present in vendor/"
     exit 1
